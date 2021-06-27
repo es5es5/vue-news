ebdb93b26752e922758b96fdedc7599c76e4c0f6
es5es5 <es5e5s@nate.com>
2021-06-27 16:19:28 +0900

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
+  "root": true,
+  "env": {
+    "node": true
   },
-  extends: [
+  "extends": [
     "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "eslint:recommended"
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    parser: "babel-eslint"
-  },
-  rules: {
-    "prettier/prettier": ["error", { "endOfLine": "auto" }],
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+  "rules": {},
+  "parserOptions": {
+    "parser": "babel-eslint"
+  }
 };
